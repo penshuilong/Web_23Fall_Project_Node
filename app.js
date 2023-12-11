64aadd0a821a56b6af1762d99fd6242c7caceeb4
zihanzhao1999 <60196990+zihanzhao1999@users.noreply.github.com>
2023-12-11 03:50:12 -0500

    Update app.js

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ const app = express()
 app.use(
     cors({
       credentials: true,
-      // origin: 'https://silly-tarsier-6acc48.netlify.app',
-      origin:"http://localhost:3000",
+      origin: 'https://silly-tarsier-6acc48.netlify.app',
+      // origin:"http://localhost:3000",
     })
    );
 
